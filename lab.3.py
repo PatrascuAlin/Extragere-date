6114e96c3348e8bc2f10d9f12374883de5a8d10f
PatrascuAlin <57074042+PatrascuAlin@users.noreply.github.com>
2019-10-27 21:45:28 +0200

    Add files via upload

---
+++ lab.3.py
@@ -0,0 +1,31 @@
+
+import urllib.request as urllib2
+from bs4 import BeautifulSoup
+import csv
+from datetime import datetime
+
+quote_pages = ['https://www.bman.ro/products/camasa-bleu-bman741',
+               'https://www.bman.ro/products/camasa-alba-cu-manseta-dubla-bman724']
+
+produse = []
+
+for page in quote_pages:
+
+    page = urllib2.urlopen(page)
+    soup = BeautifulSoup(page, 'html.parser')
+
+# extragere pret si nume produsului selectat
+    pret = soup.find('div' , attrs={'class': 'single-product-content__price-box__price'})
+    pret = pret.text.strip()
+    
+    nume = soup.find('div' , attrs={'class': 'container'})
+    nume = nume.text.strip()
+
+    produse.append((pret, nume))
+
+#adaugare informatii in fisier
+with open('bman.csv', 'a') as csv_file:
+    writer = csv.writer(csv_file)
+
+    for pret, nume in produse:
+        writer.writerow([pret, nume, datetime.now()])
